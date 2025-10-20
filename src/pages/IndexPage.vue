b8443635895d2a90bc6c4d03b7b21fd52ffa4294
kmatesa <kety.matesa@gmail.com>
2025-10-20 11:49:51 +0200

    Kreirane nove stranice

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Knjiznica"
+      src="assets/knjiznica.jpg"
+      style="width: 100px; height: 100px"
+    />
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
