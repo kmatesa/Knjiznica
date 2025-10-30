3358f5ae358c7d9320ab191274f6a572b14526ca
kmatesa <kety.matesa@gmail.com>
2025-10-30 13:48:54 +0100

    30.10

--- src/pages/PretrazivanjePage.vue
+++ src/pages/PretrazivanjePage.vue
@@ -8,6 +8,13 @@
     <div class="q-gutter-md" style="max-width: 300px">
       <q-input v-model="text" label="Pretraživanje" />
 
+    
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-checkbox dense v-model="teal" label="po nalov" color="teal" />
+       <q-checkbox dense v-model="teal" label="po autoru" color="teal" />
+ </div>
+  </div>
       <div class="q-pa-md q-gutter-sm">
     <q-btn color="blue" text-color="white" label="Traži" />
  </div>
