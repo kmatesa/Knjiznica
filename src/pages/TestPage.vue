1e09d5bd5bc10dd7f5eda8b4be540b717d90ef8b
kmatesa <kety.matesa@gmail.com>
2025-11-03 11:23:05 +0100

    Test page promjene

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -17,6 +17,10 @@
         {{ item }}
           </button>
 
+          <a :href="link.url" :target="link.target"> {{link.text}} </a>
+          <q-input color="purple-12" v-model="text" label="Item name" @keyup="log()"> </q-input>
+           <q-input color="pink-12" v-model="text" label="Item name"> </q-input>
+
      </p>
 
   </q-page>
@@ -31,5 +35,20 @@ const tijelo = "Tekst na stranici"
 const htmlText="<p><b>Formatirani tekst</b> na stranici </p>"
 const days = ['Ponedeljak', 'Utorak', 'Srijeda', 'Cetvrtak', 'Petak', 'Subota', 'Nedelja']
 
+const link = {
+url: 'http://www.google.com',
+target: '_blank',
+text: 'Google'
+}
+
+import { ref } from 'vue'
+const text = ref('')
+
+text.value = "Hello"
+
+
+function log(){
+  console.log(text.value)
+}
 
 </script>
